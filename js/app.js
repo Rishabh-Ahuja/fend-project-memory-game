111e497245dfdb6e695173c26b73c070bf87bb5d
Rishabh <rishabhahuja777@gmail.com>
2017-10-31 12:20:56 +0530

    cardLogic Function added sweetalert library and the whole game now works fine

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     "use strict";
+    let deck = $('.deck');
+    let openedCards = [];
     /*
  * Create a list that holds all of your cards
  */
@@ -12,17 +14,23 @@ $(document).ready(()=>{
      *   - add each card's HTML to the page
      */
     function init(cards) {
-        "use strict";
         cards = shuffle(cards);
         // shuffle all the cards ...
         const deck = $('.deck');
         deck.empty(); // empty all the html in deck
         // create html for cards
-        let templi = '';
-        for (let i = 0; i < cards.length; i ++) {
-            templi += `<li class="card"><i class="fa fa-${cards[i]}"></i></li>`;
+        let temp = '';
+        for (let i = 0; i < cards.length; i++) {
+            temp += `<li class="card" data-name="${cards[i]}"><i class="fa fa-${cards[i]}"></i></li>`;
         }
-        deck.html(templi);
+        deck.html(temp);
+        // show all the cards for 4 seconds
+        deck.find('li').addClass('open show');
+        // set the counter to 0
+        $('.moves').text('0');
+        setTimeout(() => {
+            deck.find('li').removeClass('open show');
+        }, 3900);
     }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -43,9 +51,140 @@ $(document).ready(()=>{
     }
 
     init(cardsList);
-console.log(cardsList);
+//* set up the event listener for a card. If a card is clicked:
+    deck.on('click', '.card', cardlogic);
+    // set up event listener for restart button
+    $('.restart').click(restart);
+
+    function cardlogic() {
+
+        // check for first time clicking
+        if (!openedCards.length) {
+            if (sameCardIsClicked.call(this)) {
+                swal("Oops.....", "Please Click A Different Card", "info");
+            }
+            showcard.call(this);
+            addcard.call(this);
+        }
+
+        else if (openedCards.length === 1) {
+
+            if (sameCardIsClicked.call(this)) {
+                swal("Oops.....", "Please Click A Different Card", "info");
+            }
+            else {
+                comparecards.call(this);
+                checkForWinningTheGame();
+
+            }
+
+        }
+
+    }
+
+    function showcard() {
+        $(this).addClass('open show');
+    }
+
+    function addcard() {
+        openedCards.push($(this).index());
+        console.log(openedCards);
+
+    }
+
+    function comparecards() {
+        // check if cards match
+        const clicked = $(this);
+        const elementToCompare = $('.deck li').get(openedCards[0]);
+        console.log(elementToCompare);
+        incrementCounter();
+        // checking if cards data-name attribute matches
+        if (clicked.attr('data-name') === $(elementToCompare).attr('data-name')) {
+            matchcards(clicked, elementToCompare); // call the matchCards function to show clicked cards and add animation
+            clearOpenArray(openedCards); // clear the openCards array for more clicking to be done
+        }
+        else {
+            // if cards do not match then hide the clicked card with some animation
+            hideCards(clicked, elementToCompare);
+        }
+    }
+
+    function matchcards(clicked, elemToCompare) {
+        clicked.addClass('open show match grow'); // animation effects
+        $(elemToCompare).addClass('open show match grow');
+        setTimeout(() => {
+            clicked.removeClass('grow');
+            $(elemToCompare).removeClass('grow');
+        }, 800); // remove the red background color ..
+    }
+
+    function sameCardIsClicked() {
+        return $(this).hasClass('open');
+    }
+
+    function hideCards(clicked, elementToCompare) {
+        clicked.addClass('swing show bg-notMatch');
+        $(elementToCompare).addClass('swing bg-notMatch');
+        setTimeout(() => {
+            clicked.removeClass('swing show bg-notMatch');
+            $(elementToCompare).removeClass('swing bg-notMatch show open')
+        }, 820);
+        // clear the open cards array index for new card matching
+        clearOpenArray(openedCards);
+    }
+
+    function clearOpenArray(array) {
+        array.shift();
+    }
+
+    function incrementCounter() {
+        const counter = $('.moves');
+        counter.text(+counter.text() + 1);
+        const stars = $('.stars');
+
+        function makestars(stars) {
+            let templi = "";
+            for (let i = 0; i < stars; i++) {
+                templi += "<li><i class=\"fa fa-star\"></i></li>"
+            }
+            return templi;
+        }
+
+        if (+counter.text() <= 10) stars.html(makestars(5));
+        else if (+counter.text() > 10 && +counter.text() <= 13) stars.html(makestars(4));
+        else if (+counter.text() > 13 && +counter.text() < 16) stars.html(makestars(3));
+        else if (+counter.text() > 16 && +counter.text() <= 19) stars.html(makestars(2));
+        else stars.html(makestars(1));
+    }
+
+    function checkForWinningTheGame() {
+        if ($('.card').length === $('.card.match').length) {
+            swal("Good job!", `You Won The game... Your Score is ${$('.moves').text()}`, "success");
+        }
+    }
+
+    function restart() {
+        swal({
+            title: "Are you sure?",
+            text: "Do You Want To Restart ?, I Know It Is Tough ... Keep Going!",
+            icon: "info",
+            buttons: true,
+        })
+            .then((willRestart) => {
+                if (willRestart) {
+                    init(cardsList);
+                    swal("Restarted! Your Game is Restarted", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("You Choose Not to Restart!", "That Is Fantastic .. Carry On...", {
+                        icon: 'success'
+                    });
+                }
+            });
+    }
+
     /*
-     * set up the event listener for a card. If a card is clicked:
      *  - display the card's symbol (put this functionality in another function that you call from this one)
      *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
      *  - if the list already has another card, check to see if the two cards match
